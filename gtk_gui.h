6e55984c933066dd604ec1823d73546e51220f5e
Agobin <agobinfolefac@gmail.com>
2016-03-14 09:06:35 +0100

    Restructured the entire code

--- gtk_gui.h
+++ gtk_gui.h
@@ -5,11 +5,11 @@
 #include "gtk_gui.h"
 
 //This function interfaces gtk_init() in chapel. It initialises gtk components to their default value
-extern void chpl_init(int *argc, const char **argv);
+void chpl_init(int *argc, const char **argv);
 
 //This functions declares a window, intilises it and then return it.
-extern GtkWidget* chpl_window_new( const char * title, int width, int height);
+GtkWidget* chpl_window_new( const char * title, int width, int height);
 
-extern void link_close_signal(GtkWidget **window);
+void link_close_signal(GtkWidget **window);
 
 #endif
